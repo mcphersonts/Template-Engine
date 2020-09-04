4364d5fe59019fbff0f3c371821b4ee97c7d278b
mcphersonts <mcphersonts@gmail.com>
2020-09-04 16:20:22 -0400

    intial prompt

--- app.js
+++ app.js
@@ -10,7 +10,19 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+function app() {
+    function createTeam() {
+        inquirer.prompt([{
+            type: "list",
+            name: "role",
+            message: "Select team member role:",
+            choices: ["Manager", "Engineer", "Intern"],
+        }])
+    }
+    createTeam()
+}
 
+app()
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
